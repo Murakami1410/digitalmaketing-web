d66c5931cc58bb7cd7e0cd34b1e0da39aa7eb672
Murakami1410 <felipe.murakami10@gmail.com>
2025-02-20 14:01:15 -0300

    Add new marketing section with BoxServices components and update SVG assets

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import BoxServices from "./components/BoxServices/BoxServices";
 import alvo from "../public/alvo.svg";
 import foguete from "../public/foguete.svg";
 import globo from "../public/globo.svg";
-import seta from "../public/Seta.svg";
+import seta from "../public/seta.svg";
 
 export default function Home() {
   return (
@@ -15,7 +15,7 @@ export default function Home() {
       <section className="h-screen flex items-center justify-center relative">
         <div className="absolute inset-0 bg-[url('https://images.unsplash.com/photo-1557264322-b44d383a2906?auto=format&fit=crop&q=80')] opacity-5 bg-cover bg-center" />
         <div className="flex flex-col items-center justify-center absolute inset-0">
-          <h1 className="text-center text-[48px] font-bold  ">
+          <h1 className="text-center text-[52px] font-bold  ">
             Transforme Sua Presença
             <span className="bg-gradient-to-r from-[#2e005c] to-[#090979] inline-block text-transparent bg-clip-text">
 
@@ -32,6 +32,71 @@ export default function Home() {
         <div className="flex flex-col items-center justify-center">
           <h1 className="font-bold text-[48px]">Nossos Serviços</h1>
           <p className="opacity-80">Oferecemos soluções abrangentes de marketing digital para ajudar sua empresa a prosperar no digital</p>
+        </div>
+        <div className="flex flex-row gap-8  mt-20 @sm:flex-col">
+
+          <BoxServices
+
+            image_component={foguete}
+            description='Estratégias digitais abrangentes adaptadas aos seus objetivos de negócios'
+            title='Estrategia Digital'
+          />
+          <BoxServices
+
+            image_component={alvo}
+            description='Envolvendo o gerenciamento de mídia social e a criação de conteúdo'
+            title='Social Media'
+          />
+
+          <BoxServices
+
+            image_component={globo}
+            description='Técnicas avançadas de SEO para melhorar sua visibilidade online'
+            title='Otimização de SEO'
+          />
+
+          <BoxServices
+
+            image_component={seta}
+            description='Análise detalhada e acompanhamento de desempenho'
+            title='Analise & Reports'
+          />
+        </div>
+      </section>
+
+      <section className="@conteiner bg-[#111827] relative mt-10 p-20 flex flex-col items-center justify-center relative ">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="font-bold text-[48px]">Porque Escolher a Gente</h1>
+          <p className="opacity-80">Oferecemos soluções abrangentes de marketing digital para ajudar sua empresa a prosperar no digital</p>
+        </div>
+        <div className="flex flex-row gap-8  mt-20 @sm:flex-col">
+
+          <BoxServices
+
+            image_component={foguete}
+            description='Estratégias digitais abrangentes adaptadas aos seus objetivos de negócios'
+            title='Estrategia Digital'
+          />
+          <BoxServices
+
+            image_component={alvo}
+            description='Envolvendo o gerenciamento de mídia social e a criação de conteúdo'
+            title='Social Media'
+          />
+
+          <BoxServices
+
+            image_component={globo}
+            description='Técnicas avançadas de SEO para melhorar sua visibilidade online'
+            title='Otimização de SEO'
+          />
+
+          <BoxServices
+
+            image_component={seta}
+            description='Análise detalhada e acompanhamento de desempenho'
+            title='Analise & Reports'
+          />
         </div>
         <div className="flex flex-row gap-8  mt-20 @sm:flex-col">
 
