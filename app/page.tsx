2c6cdd938aa284fa0cfa42cc672e0b5ec93448ef
Murakami1410 <felipe.murakami10@gmail.com>
2025-02-20 03:52:56 -0300

    Refactor homepage layout and add marketing content sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,68 @@
+"use client";
+
 import Image from "next/image";
+import React from "react";
+import CustomButtom from "./components/CustomButtom";
+import BoxServices from "./components/BoxServices/BoxServices";
+import alvo from "../public/alvo.svg";
+import foguete from "../public/foguete.svg";
+import globo from "../public/globo.svg";
+import seta from "../public/Seta.svg";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="">
+      <section className="h-screen flex items-center justify-center relative">
+        <div className="absolute inset-0 bg-[url('https://images.unsplash.com/photo-1557264322-b44d383a2906?auto=format&fit=crop&q=80')] opacity-5 bg-cover bg-center" />
+        <div className="flex flex-col items-center justify-center absolute inset-0">
+          <h1 className="text-center text-[48px] font-bold  ">
+            Transforme Sua Presença
+            <span className="bg-gradient-to-r from-[#2e005c] to-[#090979] inline-block text-transparent bg-clip-text">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+              Digital
+
+            </span>
+          </h1>
+          <p className="text-center w-1/3 opacity-80">Criamos estratégias de marketing digital de ponta que geram resultados e cresce seu negócio</p>
+          <CustomButtom title="Saiba Mais" className="mt-10 p-3 font-bold text-white rounded-full border-2 hover:border-2 hover:border-[blue] hover:text-blue hover:p-3 hover:rounded-full transition-all ease-in delay-75" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+      </section>
+
+      <section className="@conteiner relative mt-10 p-20 flex flex-col items-center justify-center relative ">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="font-bold text-[48px]">Nossos Serviços</h1>
+          <p className="opacity-80">Oferecemos soluções abrangentes de marketing digital para ajudar sua empresa a prosperar no digital</p>
+        </div>
+        <div className="flex flex-row gap-8  mt-20 @sm:flex-col">
+
+          <BoxServices
+
+            image_component={foguete}
+            description='Estratégias digitais abrangentes adaptadas aos seus objetivos de negócios'
+            title='Estrategia Digital'
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+          <BoxServices
+
+            image_component={alvo}
+            description='Envolvendo o gerenciamento de mídia social e a criação de conteúdo'
+            title='Social Media'
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
+
+          <BoxServices
+
+            image_component={globo}
+            description='Técnicas avançadas de SEO para melhorar sua visibilidade online'
+            title='Otimização de SEO'
           />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+          <BoxServices
+
+            image_component={seta}
+            description='Análise detalhada e acompanhamento de desempenho'
+            title='Analise & Reports'
+          />
+        </div>
+      </section>
     </div>
   );
 }
